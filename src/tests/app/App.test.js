6475aec71c69b9777e7517e22d0063df29e93639
Burak Kara <git-8f4cb@burak-kara.dev>
2021-03-25 00:19:49 +0300

    test fix

--- src/tests/app/App.test.js
+++ src/tests/app/App.test.js
@@ -1,10 +1,15 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
 import '@testing-library/jest-dom';
+import { BrowserRouter as Router } from 'react-router-dom';
 import App from '../../pages/app/App';
 
 test('renders account button', () => {
-    render(<App />);
+    render(
+        <Router>
+            <App />
+        </Router>,
+    );
     const linkElement = screen.getByText(/John/i);
     expect(linkElement).toBeInTheDocument();
 });
