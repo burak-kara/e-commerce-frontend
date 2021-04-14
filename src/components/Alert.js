1a4a935b26c881f9f1fafd42e290471e9e9101d5
Burak Kara <git-8f4cb@burak-kara.dev>
2021-04-14 18:29:18 +0300

    added Alert component.

---
+++ src/components/Alert.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import MuiAlert from '@material-ui/lab/Alert';
+import Snackbar from '@material-ui/core/Snackbar';
+
+const MuiCustomAlert = (props) => <MuiAlert elevation={6} variant="filled" {...props} />;
+
+const Alert = (props) => {
+    const { open, duration, handleClose, severity, message } = props;
+
+    return (
+        <Snackbar
+            anchorOrigin={{ vertical: 'top', horizontal: 'right' }}
+            open={open}
+            autoHideDuration={duration || 2000}
+            onClose={handleClose}
+        >
+            <MuiCustomAlert onClose={handleClose} severity={severity}>
+                {message}
+            </MuiCustomAlert>
+        </Snackbar>
+    );
+};
+
+export default Alert;
