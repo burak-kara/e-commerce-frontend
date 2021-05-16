8abd08ec8596e469699c2490ac13ac991275e97c
Burak Kara <git-8f4cb@burak-kara.dev>
2021-05-16 22:45:02 +0300

    added server workers to convert app to PWA.

---
+++ public/serviceWorker.js
@@ -0,0 +1,26 @@
+const CACHE_NAME = 'my-site-cache-v1';
+const urlsToCache = ['/', '/index.html'];
+
+// eslint-disable-next-line no-restricted-globals
+self.addEventListener('install', (event) => {
+    // Perform install steps
+    event.waitUntil(
+        caches.open(CACHE_NAME).then((cache) => {
+            return cache.addAll(urlsToCache);
+        }),
+    );
+    // eslint-disable-next-line no-restricted-globals
+    self.skipWaiting();
+});
+
+// eslint-disable-next-line no-restricted-globals
+self.addEventListener('fetch', (event) => {
+    event.respondWith(
+        caches.match(event.request).then((response) => {
+            if (response) {
+                return response;
+            }
+            return fetch(event.request);
+        }),
+    );
+});
